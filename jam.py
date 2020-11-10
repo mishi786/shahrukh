72f1c8e4a3abc1ed6a56a40cea05c6feb6fa55de
mishi786 <68304964+mishi786@users.noreply.github.com>
2020-11-10 23:58:31 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -523,10 +523,10 @@ t "\033[1;95m•◈•▬ ▬ ▬•◈\033[1;91mJAM-SHAHRUKH\033[1;95m◈•▬
 	print "  \033[1;91m«---•◈•---Developed By JAM-SHAHRUKH--•◈•---»" #Dev:Jam
 	print '\033[1;93m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (python2 Mishal.py)↩\033[1;97m....'
 	print"\033[1;91mTotal OK/\x1b[1;95mCP \033[1;93m: \033[1;91m"+str(len(oks))+"\033[1;93m/\033[1;96m"+str(len(cekpoint))
-	print """
-	
-	raw_input("\n\033[1;95m[\033[1;91mBack\033[1;95m]")
-	menu()
+	print " \033[1;36;40m●════════════════════════☠════════════════════════●\n"
+		os.system('git pull origin master')
+		raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
+		menu()
 
 if __name__ == '__main__':
 	login()
